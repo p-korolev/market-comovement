19b16c2e515c75a84f0654a802973c28042e0844
Phillip Korolev <p.korolev1@outlook.com>
2025-08-22 13:06:54 -0400

    Add files via upload

--- analytics.py
+++ analytics.py
@@ -1,89 +1,67 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from typing import Union, List, Optional
 
+from typing import Union, Any
+from numbers import Real 
+
+import stats
+import algebra
 from instrument import Priceable
-import algebra as algebra
 
-class Display():
-    def __init__(self, plotcount: int):
-        self.plotcount = plotcount
-        self.fig, self.display = plt.subplots(plotcount)
+# base linear plot class
+class Plot:
+    def __init__(self, *series: pd.Series):
+        for s in series:
+            plt.plot(s)
     
-    def plot(self, 
-             plot_index: int, 
-             *data: Union[List, pd.Series]) -> None:
-        '''
-        Plots data into display index passed. Pass multiple pd.Series objects to plot x-axis respective series together.
+    def show(self) -> None:
+        plt.legend()
+        return plt.show()
 
-        :param plot_index: index of plot display to plot data.
-        :param data: either x,y data pair lists or 1+ pd.Series objects.
-        '''
-
-        # plot x,y data pair if data has 2 args
-        if len(data)==2 and (isinstance(data[0], List) and isinstance(data[1], List)):
-            
-            self.display[plot_index].plot(data[0], data[1])
-            return True
-        
-        plotted = 0
-        if len(data)==1 and (isinstance(data[0], pd.Series)):
-            self.display[plot_index].plot(data[0])
-            return True
-    
-        # else assume data args are type pd.Series
-        i = 0
-        for dataset in data:
-            # check for no reason
-            if isinstance(dataset, pd.Series):
-                self.display[plot_index].plot(dataset)
-            plotted+=1
-            i+=1
-        
-        print(f"{plotted}/{len(data)} data series plotted at display index {plot_index}.")
-        return True
-    
-    def show(self):
-        self.fig.show()
-        input("Press any key to close plot.")
-
-class ComparativeModel(Display):
+class PricePlot:
     def __init__(self, 
-                 primary_derivative: Priceable,
-                 secondary_derivative: Priceable,
-                 date_period: str,
-                 time_interval: str,
-                 price_timing: str,
-                 xy_split: bool = False,
-                 momentum_indicators: Optional[bool] = False, 
-                 momentum_bias: Optional[int] = None):
-        super().__init__(plotcount=3)
-        self.der1, self.der2 = primary_derivative, secondary_derivative
-        self.period = date_period
-        self.interval = time_interval
-        self.der1_name, self.der2_name = primary_derivative.symbol, secondary_derivative.symbol
+                 *ticks: str, 
+                 price_timing: str = 'Open',
+                 period: str = '1d',
+                 interval: str = '1m'):
+        self.price_series = []
+        for tick in ticks:
+            self.price_series.append(
+                Priceable(type='stock', name_symbol=tick).get_price_history(price_timing=price_timing, period=period, interval=interval)
+                )
+        i=0
+        for s in self.price_series:
+            plt.plot(s, label=ticks[i])
+            i+=1
+    
+    def show(self) -> None:
+        plt.legend()
+        plt.show()
 
-        # populate primary derivative price series
-        self.der1_prices = primary_derivative.get_price_history(price_timing=price_timing, period=date_period, interval=time_interval)
-        # in case of individual x,y plotting
-        if xy_split:
-            self.d1x = self.der1_prices.index.tolist()
-            self.d1y = self.der1_prices.values.tolist()
+class ScaledPricePlot():
+    def __init__(self, 
+                 *ticks: str, 
+                 price_timing: str = 'Open',
+                 period: str = '1d',
+                 interval: str = '1m',
+                 scale_start: Real = 100):
+        self.price_series = []
+        for tick in ticks:
+            self.price_series.append(algebra.scale(
+                Priceable(type='stock', name_symbol=tick).get_price_history(price_timing=price_timing, period=period, interval=interval),
+                initial=scale_start)
+                )
+        i=0
+        for s in self.price_series:
+            plt.plot(s, label=ticks[i])
+            i+=1
+    
+    def show(self) -> None:
+        plt.legend()
+        plt.show()
 
-        # populate secondary derivative price series
-        self.der2_prices = secondary_derivative.get_price_history(price_timing=price_timing, period=date_period, interval=time_interval)
-        if xy_split:
-            self.d2x = self.der2_prices.index.tolist()
-            self.d2y = self.der2_prices.values.tolist()
-        
-        # get normalized series
-        self.der1_prices_normalized = algebra.normalize(self.der1_prices)
-        self.der2_prices_normalized = algebra.normalize(self.der2_prices)
-        
-        if momentum_indicators:
-            self.mmt_bias = momentum_bias
+p = ScaledPricePlot('XOM', 'CVX', 'AAPL')
+p.show()
 
-        else:
-            self.plot()
         
---
+++ stats.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import numpy as np
+
+from typing import Union
+from numbers import Real 
+
+def covariance(x: Union[pd.Series, np.ndarray], y: Union[pd.Series, np.ndarray]) -> Real:
+    '''
+    Returns the covariance between two series of data as real.
+
+    :param x: data series of reals
+    :param y: data series of reals
+
+    **Examples**
+    
+    >>> xom = Priceable(type='stock', name_symbol='XOM').get_price_history(price_timing='Close', period='1mo')
+    >>> cvx = Priceable(type='stock', name_symbol='CVX').get_price_history(price_timing='Close', period='1mo')
+    >>> covariance(xom, cvx)
+    1.6887381204433043
+    
+    >>> xom = Priceable(type='stock', name_symbol='XOM').get_price_history(price_timing='Close', period='1mo')
+    >>> shel = Priceable(type='stock', name_symbol='SHEL').get_price_history(price_timing='Close', period='1mo')
+    >>> covariance(xom, shel)
+    0.5991333231946371
+    '''
+    return np.cov(x, y)[0][1]
+
+def cov_matrix(*x: Union[pd.Series, np.ndarray]) -> np.ndarray[Real]:
+    '''
+    Returns the covariance matrix of multiple data series of reals.
+
+    :param *x: series of reals
+
+    **Examples**
+    
+    >>> xom = Priceable(type='stock', name_symbol='XOM').get_price_history(price_timing='Close', period='1mo')
+    >>> cvx = Priceable(type='stock', name_symbol='CVX').get_price_history(price_timing='Close', period='1mo')
+    >>> shel = Priceable(type='stock', name_symbol='SHEL').get_price_history(price_timing='Close', period='1mo')
+    >>> covariance_matrix(xom, cvx, shel)
+    [[4.5737686  1.61292396 0.60793976]
+     [1.61292396 4.99476267 1.0364035 ]
+     [0.60793976 1.0364035  0.38670407]]
+    '''
+    if len(x)>2:
+        return np.cov(np.vstack([series for series in x]))
+    return np.cov(*x)
+
+def correlation_matrix(*x: Union[pd.Series, np.ndarray]) -> np.ndarray[Real]:
+    '''
+    Returns the normalized correlation matrix of multiple data series of reals.
+
+    :param *x: series of reals
+
+    **Examples**
+    
+    >>> xom = Priceable(type='stock', name_symbol='XOM').get_price_history(price_timing='Close', period='1mo')
+    >>> cvx = Priceable(type='stock', name_symbol='CVX').get_price_history(price_timing='Close', period='1mo')
+    >>> shel = Priceable(type='stock', name_symbol='SHEL').get_price_history(price_timing='Close', period='1mo')
+    >>> covariance_matrix(xom, cvx, shel)
+    [[1.         0.33268224 0.45317276]
+     [0.33268224 1.         0.74402584]
+     [0.45317276 0.74402584 1.        ]]
+    '''
+    cov = cov_matrix(*x)
+    std_outer = np.outer((np.sqrt(np.diag(cov))), (np.sqrt(np.diag(cov))))
+    return cov/std_outer
+
+def correlation(x: Union[pd.Series, np.ndarray], y: Union[pd.Series, np.ndarray]) -> Real:
+    '''
+    Returns the normalized correlation of two series of reals.
+
+    :param x: series of reals
+    :param y: series of reals
+
+    **Examples**
+    
+    >>> xom = Priceable(type='stock', name_symbol='XOM').get_price_history(price_timing='Close', period='1mo')
+    >>> cvx = Priceable(type='stock', name_symbol='CVX').get_price_history(price_timing='Close', period='1mo')
+    >>> correlation(x=xom, y=cvx)
+    0.33420128794152715
+    '''
+    return correlation_matrix(x, y)[0][1]
+
+def variance(x: Union[pd.Series, np.ndarray]) -> Real:
+    '''
+    Returns the variance of a series of reals.
+
+    :param x: series of reals
+
+    **Examples**
+
+    >>> xom = Priceable(type='stock', name_symbol='XOM').get_price_history(price_timing='Close', period='1mo')
+    >>> variance(xom)
+    4.36387949368984
+    '''
+    return np.var(x)
