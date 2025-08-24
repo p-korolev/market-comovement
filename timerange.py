e65ec25e3782315c3b00ff9cfa0ede134a4964c8
Phillip Korolev <p.korolev1@outlook.com>
2025-08-24 10:58:58 -0400

    Create timerange.py

---
+++ timerange.py
@@ -0,0 +1,24 @@
+class Interval(str, Enum):
+    MINUTE = '1m'
+    TWO_MINUTE = '2m'
+    FIVE_MINUTE = '5m'
+    HOUR = '1h'
+    DAY = '1d'
+    FIVE_DAY = '5d'
+    WEEK = '7d'
+    MONTH = '1mo'
+    THREE_MONTH = '3mo'
+
+class Period(str, Enum):
+    DAY = '1d'
+    FIVE_DAY = '5d'
+    MONTH = '1mo'
+    THREE_MONTH = '3mo'
+    SIX_MONTH = '6mo'
+    YEAR = '1y'
+    TWO_YEAR = '2y'
+    FIVE_YEAR = '5y'
+    TEN_YEAR = '10y'
+    YTD = 'ytd'
+    MAX = 'max'
+    
